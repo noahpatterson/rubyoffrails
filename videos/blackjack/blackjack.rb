5d122ce6722d693bc0ba0f12b4b2db3a32f48434
noahpatterson <noah@noah-patterson.com>
2013-12-04 06:58:27 -0500

    add rspec videos and assignment, add blackjack video and testing

--- videos/blackjack/blackjack.rb
+++ videos/blackjack/blackjack.rb
@@ -1,12 +1,29 @@
+require '../../spec_helper'
+
+
 class Card
-	def value
-		5
+
+	attr_reader :suit, :value
+
+	def initialize(suit, value)
+		@suit = suit
+		@value = value
 	end
+
+
 end
 
-describle Card do 
-	it "should have a value of 6" do
-		# Card.new.value.should eq(6)
-		value = Card.new
-		value.value eq(6)
+describe Card do 
+	it "should have a suit" do
+		expect(Card.new(:clubs,10).suit).not_to be_nil
+	end
+	it "should have a value" do
+		expect(Card.new(:hearts, 2).value).not_to be_nil
+	end
+
+	it "should accept suit and value when building" do
+		card = Card.new(:clubs,10)
+		expect(card.suit).to eq(:clubs)
+		expect(card.value).to eq(10)
+	end
 end
